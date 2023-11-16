ea53c46c69900d206f2fee9de9a0690ff03b316c
Patricia <patriciastarck@gmail.com>
2023-11-16 07:40:38 -0300

    feat: project created

---
+++ proj-dslist/src/main/java/com/devs/proj/ProjDslistApplication.java
@@ -0,0 +1,13 @@
+package com.devs.proj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjDslistApplication.class, args);
+	}
+
+}
